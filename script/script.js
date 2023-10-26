c52eb92470164ea338da9de047f325ab1b287009
satz15 <sathishsekar154@gmail.com>
2023-10-26 13:48:54 +0530

    completed

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// Using javascript file as strict file for exposing all the loss errors or eliminating the silent errors into throwable errors
+'use strict';
+
+//selecting every elements from the html files in two types inputs(providing values) and outputs(displaying)
+//inputs
+
+//outputs
+
+//global variable
+
+//functions
+const init = ()=> {}
+
+//events
+
+//initial settings
+init();
